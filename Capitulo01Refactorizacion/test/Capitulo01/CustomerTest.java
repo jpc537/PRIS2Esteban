60d3d51ef557a7d861fe579ed48a37b5e4e34dd8
Esteban <jpc537@inlumine.ual.es>
2016-04-11 18:04:53 +0200

    Inicio CustomerTest.

---
+++ Capitulo01Refactorizacion/test/Capitulo01/CustomerTest.java
@@ -0,0 +1,34 @@
+package Capitulo01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomerTest {
+
+	@Test
+	public void testCustomer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddRental() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStatement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHtmlStatement() {
+		fail("Not yet implemented");
+	}
+
+}

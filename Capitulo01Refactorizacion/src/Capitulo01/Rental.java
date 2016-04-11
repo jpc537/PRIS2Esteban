dd6ced62a13af72a3ff9d0736c81368138f1cb54
Esteban <jpc537@inlumine.ual.es>
2016-04-11 17:42:22 +0200

    Cuarta refactorización. Removing Temps

--- Capitulo01Refactorizacion/src/Capitulo01/Rental.java
+++ Capitulo01Refactorizacion/src/Capitulo01/Rental.java
@@ -18,23 +18,24 @@ public class Rental {
 	}
 
 	public double getCharge() {
-		double result = 0;
+		double thisAmount = 0;
 		switch (getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (getDaysRented() > 2)
-				result += (getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (getDaysRented() > 3)
-				result += (getDaysRented() - 3) * 1.5;
-			break;
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (getDaysRented() > 2)
+				thisAmount += (getDaysRented() - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += getDaysRented() * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (getDaysRented() > 3)
+					thisAmount += (getDaysRented() - 3) * 1.5;
+				break;
+			default: break;
 		}
-		return result;
+		return thisAmount;
 	}
 
 	public int getFrequentRenterPoints() {

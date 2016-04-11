17b7f4f82f6137e8889b07cabcc577a256e22cfa
Esteban <jpc537@inlumine.ual.es>
2016-04-11 18:50:14 +0200

    Continuación con los test.

--- Capitulo01Refactorizacion/test/Capitulo01/CustomerTest.java
+++ Capitulo01Refactorizacion/test/Capitulo01/CustomerTest.java
@@ -1,34 +1,30 @@
 package Capitulo01;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class CustomerTest {
 
-	@Test
-	public void testCustomer() {
-		fail("Not yet implemented");
-	}
+    private static final Movie EL_RENACIDO = new Movie("El Renacido", Movie.CHILDREN);
+   
+    private final Customer customer = new Customer("Esteban");
 
-	@Test
-	public void testGetName() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void basicChildrenRental() {
+        customer.addRental(new Rental(EL_RENACIDO, 2));
+        assertThat(customer.statement(), is(expectedMessageFor("El Renacido", 1.5, 1.5, 1)));
+    }
 
-	@Test
-	public void testAddRental() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testStatement() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testHtmlStatement() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void shouldDiscountChildrensRentals() {
+        customer.addRental(new Rental(EL_RENACIDO, 4));
+        assertThat(customer.statement(), is(expectedMessageFor("El Renacido", 3.0, 3.0, 1)));
+    }
+  
+    private static String expectedMessageFor(String rental, double price, double total, int renterPointsEarned) {
+        return "Rental record for Esteban\n\t" + rental + "\t" + price + "\nAmount owed is " + total + "\nYou earned " + renterPointsEarned + " frequent renter points";
+    }
 
 }
